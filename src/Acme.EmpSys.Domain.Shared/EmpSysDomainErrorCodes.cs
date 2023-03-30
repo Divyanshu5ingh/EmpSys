07128cad05a151577e1a3293f230f516c3b366e7
Divu <d1403kr3@gmail.com>
2023-03-30 14:40:49 +0530

    Step 6

--- src/Acme.EmpSys.Domain.Shared/EmpSysDomainErrorCodes.cs
+++ src/Acme.EmpSys.Domain.Shared/EmpSysDomainErrorCodes.cs
@@ -3,4 +3,5 @@
 public static class EmpSysDomainErrorCodes
 {
     /* You can add your business exception error codes here, as constants */
+    public const string DepartmentAlreadyExists = "EmpSys:00001";
 }
